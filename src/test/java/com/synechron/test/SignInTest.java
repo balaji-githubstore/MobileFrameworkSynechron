534fe32b1856eb12060993ffea28180446ae05cc
balaji-githubstore <balaji0017@gmail.com>
2022-01-24 12:51:54 +0530

    beforemethod and aftermethod created in signintest

--- src/test/java/com/synechron/test/SignInTest.java
+++ src/test/java/com/synechron/test/SignInTest.java
@@ -8,15 +8,18 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class SignInTest {
+	AndroidDriver<WebElement> driver;
 
-	@Test
-	public void invalidCredentialTest() throws MalformedURLException {
+	@BeforeMethod
+	public void startApp() throws MalformedURLException {
 		File file = new File("src/test/resources/app/khan-academy-7-3-2.apk");
 		String absolutePath = file.getAbsolutePath();
 		System.out.println(absolutePath);
@@ -26,9 +29,17 @@ public class SignInTest {
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		cap.setCapability(MobileCapabilityType.APP, absolutePath);
 
-		AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(new URL("http://localhost:4723/wd/hub"), cap);
-
+		driver = new AndroidDriver<WebElement>(new URL("http://localhost:4723/wd/hub"), cap);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@AfterMethod
+	public void terminateApp() {
+		driver.quit();
+	}
+
+	@Test
+	public void invalidCredentialTest() throws MalformedURLException {
 
 		driver.findElementByXPath("//*[@text='Dismiss']").click();
 		driver.findElementByXPath("//*[@text='Sign in']").click();
@@ -42,7 +53,7 @@ public class SignInTest {
 
 		driver.findElementByXPath("(//*[@text='Sign in'])[2]").click();
 		String actualError = driver.findElementByXPath("//*[contains(@text,'issue')]").getText();
-		
+
 		Assert.assertEquals(actualError, "There was an issue signing in");
 	}
 
